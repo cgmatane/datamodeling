9e84ab575d4d03e502f5b53ccea67bfb20f3d263
BoHuny <BoHuny@hotmail.com>
2019-09-28 17:28:51 -0400

    Ajout heure avec date pour les champs timestamp des entrees de la table programmation

--- SCRIPT INSERTION DONNEES.sql
+++ SCRIPT INSERTION DONNEES.sql
@@ -24,10 +24,10 @@ INSERT INTO public.trajet (id_station_depart, id_station_arrivee, id_navire)
 
 
 INSERT INTO public.programmation (id_trajet, date_arrivee, date_depart, nom, annulation)
-  VALUES (1 , '2019-10-12' , '2019-10-11' , 'Baie-Comeau Matane', False);
+  VALUES (1 , '2019-10-12' , '2019-10-11 15:30:00' , 'Baie-Comeau Matane', False);
 
 INSERT INTO public.programmation (id_trajet, date_arrivee, date_depart, nom, annulation)
-  VALUES (2, '2019-10-14', '2019-10-14', 'Godbout Baie-Comeau', False);
+  VALUES (2, '2019-10-14', '2019-10-14 11:00:00', 'Godbout Baie-Comeau', False);
 
 
 INSERT INTO public.passager (nom, prenom, date_naissance)
