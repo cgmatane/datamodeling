3704a1df246ac53df279edad14df22d0ec78f0be
Raphaël Lavoie <rapolysec@hotmail.com>
2019-11-07 15:17:26 -0500

    Insertion Données corrigée

--- Version #2/SCRIPT INSERTION DONNEES V2.sql
+++ Version #2/SCRIPT INSERTION DONNEES V2.sql
@@ -13,20 +13,17 @@ INSERT INTO public.station (nom)
 INSERT INTO public.station (nom)
   VALUES ('Godbout');
 
+INSERT INTO public.ticket(id_trajet, prix, qrcode, date_achat, nombre_bagage, nombre_animal, telephone, mail)
+  VALUES(1, 300, 'AHEIFBG', '2019-10-10' , 0 , 1, 4187456680, 'troisdeuxun@haha.com');
+
+INSERT INTO public.ticket(id_trajet, prix, qrcode, date_achat, nombre_bagage, nombre_animal, telephone, mail)
+  VALUES(2, 300, 'UGJANGT' , '2019-10-12' , 10 , 0 , 2, 4185551982, 'undeuxtrois@haha.com');
+  
 INSERT INTO public.trajet (id_station_depart, id_station_arrivee, id_navire, date_depart, date_arrivee, nom, annulation)
   VALUES (2, 3 , 1, '2019-10-11 15:30:00', '2019-10-12 08:00:00', 'Baie-Comeau Matane', False);
 
 INSERT INTO public.trajet (id_station_depart, id_station_arrivee, id_navire, date_depart, date_arrivee, nom, annulation)
   VALUES (3 , 2 , 1, '2019-10-14 11:00:00', '2019-10-14 17:00:00', 'Godbout Baie-Comeau', False);
-
-INSERT INTO public.passager (nom, prenom, date_naissance, id_ticket)
-  VALUES ('Dufour' , 'Judith' , '1996-05-22', 1);
-
-INSERT INTO public.passager (nom, prenom, date_naissance, id_ticket)
-  VALUES ('Poirier' , 'Guy', '1986-10-10', 1 );
-
-INSERT INTO public.passager (nom, prenom, date_naissance, id_ticket)
-  VALUES ('Johnson' , 'Felix', '1999-02-04', 2);
 
 INSERT INTO public.type_vehicule (nom, prix_type_vehicule)
   VALUES ('Voiture', 80);
@@ -45,12 +42,6 @@ INSERT INTO public.vehicule (type_vehicule, couleur, immatriculation, modele, ma
 
 INSERT INTO public.vehicule (type_vehicule, couleur, immatriculation, modele, marque, id_ticket)
   VALUES (2, 'Vert', '933KC', 'Berline', 'Mercedes', 2);
-
-INSERT INTO public.ticket(id_trajet, prix, qrcode, date_achat, nombre_bagage, nombre_animal, telephone, mail)
-  VALUES(1, 300, 1, 'AHEIFBG', '2019-10-10' , 0 , 1, 4187456680, 'troisdeuxun@haha.com');
-
-INSERT INTO public.ticket(id_trajet, prix, qrcode, date_achat, nombre_bagage, nombre_animal, telephone, mail)
-  VALUES(2, 300, 2, 'UGJANGT' , '2019-10-12' , 10 , 0 , 2, 4185551982, 'undeuxtrois@haha.com');
 
 INSERT INTO public.users(id, name, email, email_verified_at, password, remember_token, created_at,updated_at)
   VALUES(1,'Mr. Trever Cole V','kirk.stark@example.net','2019-10-04 21:18:52', '$2y$10$cyWC4h5YU0QPYlidcGzjZ.gkI41RRklwD23XgGGhmDTjEzaqxQtae', 'hwHQnxo9Gt', '2019-10-04 21:18:53', '2019-10-04 21:18:53' );
