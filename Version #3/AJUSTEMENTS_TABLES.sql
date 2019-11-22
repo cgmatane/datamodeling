e5218af36b8ac18051b8a4ad876b489c700b409c
Benjamin Boullé <1934904@cgmatane.qc.ca>
2019-11-21 21:21:03 -0500

    Ajout script BDD V3

---
+++ Version #3/AJUSTEMENTS_TABLES.sql
@@ -0,0 +1,45 @@
+
+CREATE SEQUENCE public.id_intervalle_age_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.intervalle_age
+(
+    id_intervalle_age integer NOT NULL DEFAULT nextval('id_intervalle_age_seq'::regclass),
+    age_min integer NOT NULL,
+    age_max integer NOT NULL,
+    tarif numeric(6,2),
+    CONSTRAINT intervalle_age_pkey PRIMARY KEY (id_intervalle_age)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.intervalle_age
+    OWNER to postgres;
+
+INSERT INTO public.intervalle_age(age_min, age_max, tarif) VALUES (0,17,1500);
+INSERT INTO public.intervalle_age(age_min, age_max, tarif) VALUES (18,60,4000);
+INSERT INTO public.intervalle_age(age_min, age_max, tarif) VALUES (61,999,35);
+
+ALTER TABLE public.ticket DROP COLUMN nombre_bagage;
+ALTER TABLE public.ticket DROP COLUMN nombre_animal;
+
+ALTER TABLE public.ticket ADD COLUMN numero_facture integer;
+ALTER TABLE public.ticket ADD COLUMN commentaires varchar;
+UPDATE public.ticket SET numero_facture = 0 WHERE True;
+UPDATE public.ticket SET commentaires = 'N/A' WHERE True;
+ALTER TABLE public.ticket ALTER COLUMN numero_facture SET NOT NULL;
+ALTER TABLE public.ticket ALTER COLUMN commentaires SET NOT NULL;
+
+ALTER TABLE public.type_vehicule RENAME COLUMN prix_type_vehicule TO tarif;
+
+ALTER TABLE public.passager ADD COLUMN id_intervalle_age integer;
+UPDATE public.passager SET id_intervalle_age = 1 WHERE True;
+ALTER TABLE public.passager ALTER COLUMN id_intervalle_age SET NOT NULL;
+
+ALTER TABLE ONLY public.passager ADD CONSTRAINT intervalle_age_passager_fk FOREIGN KEY (id_intervalle_age) REFERENCES public.intervalle_age(id_intervalle_age) ON UPDATE CASCADE ON DELETE CASCADE;
