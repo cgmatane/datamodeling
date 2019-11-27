ffb20415072fa1133a82c6bb2aeb5ca6188bdd1f
Raphaël Lavoie <rapolysec@hotmail.com>
2019-11-27 08:31:04 -0500

    Nouveaux scripts pour les scans

---
+++ Version #3/NOUVELLE_TABLE_SCAN.sql
@@ -0,0 +1,16 @@
+CREATE TABLE scan (
+   	id_scan int,
+	id_ticket int,
+	id_trajet int,
+	qr_code varchar(7),
+	date_achat varchar(10),
+	telephone varchar(10),
+	commentaires varchar,
+	relation_passagers text,
+	relation_vehicule text
+	
+)
+
+INSERT INTO public.scan(
+	id_scan, id_ticket, id_trajet, qr_code, date_achat, telephone, commentaires, relation_passagers, relation_vehicule)
+	VALUES (1, null, null, '', '', '', '', '{}', '{}');
