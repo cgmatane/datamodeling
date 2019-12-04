1a2dd5b2952305b62ea9a492fbc96793900d65e2
lchateau <44263204+lchateau@users.noreply.github.com>
2019-12-04 17:45:18 -0500

    modification des droits pour interface admin

---
+++ UPDATE_PERMISSION.sql
@@ -0,0 +1,3 @@
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO stqmatane;
+GRANT INSERT, UPDATE, DELETE ON station, navire, trajet, intervalle_age, type_vehicule TO stqmatane;
+GRANT INSERT, UPDATE ON ticket TO stqmatane;
