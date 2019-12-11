764108cac1b103ce1c08730de926fe6c977bf864
Lucas Chateau <44263204+lchateau@users.noreply.github.com>
2019-12-11 17:48:41 -0500

    Changement des permissions

--- UPDATE_PERMISSION.sql
+++ UPDATE_PERMISSION.sql
@@ -1,3 +1,2 @@
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO stqmatane;
-GRANT INSERT, UPDATE, DELETE ON station, navire, trajet, intervalle_age, type_vehicule TO stqmatane;
-GRANT INSERT, UPDATE ON ticket TO stqmatane;
+GRANT INSERT, UPDATE, DELETE ON station, navire, trajet, intervalle_age, type_vehicule, ticket, vehicule, passager TO stqmatane;
